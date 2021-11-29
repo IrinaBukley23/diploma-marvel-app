99b8cae93394544463ccb015328214dc81eb7efb
Irina <irenserge@mail.ru>
2021-11-29 21:28:02 +0300

    feat: char-info is created

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,25 +1,39 @@
-
+import {Component} from 'react';
 import Header from './components/Header';
 import RandomChar from './components/RandomChar';
 import CharList from './components/CharList';
+import CharInfo from './components/CharInfo';
 import './styles/variables.scss';
 
 import decoration from './resources/vision.png';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <main>
-        <RandomChar/>
-        <div className="char__content">
-          <CharList/>
+class App extends Component {
+  
+  state = {
+    selectedChar: null,
+  }
 
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision"/>
-      </main>
-    </div>
-  );
+  onCharSelected = (id) => {
+    this.setState({
+      selectedChar: id
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <main>
+          <RandomChar/>
+          <div className="char__content">
+            <CharList onCharSelected={this.onCharSelected}/>
+            <CharInfo charId={this.state.selectedChar}/>
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision"/>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- marvel/src/components/RandomChar/RandomChar.jsx
+++ marvel/src/components/RandomChar/RandomChar.jsx
@@ -41,8 +41,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        this.marvelService
-            .getCharacter(id)
+        this.marvelService.getCharacter(id)
             .then(this.onCharLoaded)
             .catch(this.onError);
     }
@@ -54,13 +53,22 @@ class RandomChar extends Component {
         const spinner = loading ? <Spinner/> : null;
         const errorMessage = error ? <ErrorMessage/> : null;
 
+        let imgStyle = {'objectFit' : 'cover'};
+        if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+            imgStyle = {'objectFit' : 'contain'};
+        }
+
         return (
             <div className="randomchar">
                 {spinner}
                 {errorMessage}
                 { !(loading | error) ? (
                     <div className="randomchar__block">
-                        <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                        <img 
+                            src={thumbnail} 
+                            alt="Random character" 
+                            style={imgStyle}
+                            className="randomchar__img"/>
                         <div className="randomchar__info">
                             <p className="randomchar__name"> {name} </p>
                             <p className="randomchar__descr">
