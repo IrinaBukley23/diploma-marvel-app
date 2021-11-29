fbdf4bc6bb48dfdde198098a6b654f260225952f
Irina <irenserge@mail.ru>
2021-11-29 16:29:55 +0300

    feat: add spinner

--- marvel/src/components/RandomChar/RandomChar.jsx
+++ marvel/src/components/RandomChar/RandomChar.jsx
@@ -1,5 +1,5 @@
 import {Component} from 'react';
-
+import Spinner from '../Spinner';
 import MarvelService from '../../services/MarvelService';
 
 import './RandomChar.scss';
@@ -13,13 +13,18 @@ class RandomChar extends Component {
     }
 
     state = {
-        char: {}
+        char: {},
+        loading: true,
     }
 
     marvelService = new MarvelService();
     
     onCharLoaded = (char) => {
-        this.setState( {char} );
+        this.setState( {
+            char,
+            loading: false,
+        
+            } );
     }
 
     updateChar = () => {
@@ -31,27 +36,33 @@ class RandomChar extends Component {
 
     render () {
 
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        const {char: {name, description, thumbnail, homepage, wiki}, loading} = this.state;
+
+        const spinner = loading ? <Spinner/> : null;
 
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name"> {name} </p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
+                {spinner}
+                { !(loading) ? (
+                    <div className="randomchar__block">
+                        <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                        <div className="randomchar__info">
+                            <p className="randomchar__name"> {name} </p>
+                            <p className="randomchar__descr">
+                                {description}
+                            </p>
+                            <div className="randomchar__btns">
+                                <a href={homepage} className="button button__main">
+                                    <div className="inner">homepage</div>
+                                </a>
+                                <a href={wiki} className="button button__secondary">
+                                    <div className="inner">Wiki</div>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ) : null }
+               
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -62,7 +73,7 @@ class RandomChar extends Component {
                     </p>
                     <button 
                         className="button button__main"
-                        //onClick={this.updateChar}
+                        onClick={this.updateChar}
                         >
                         <div className="inner">try it</div>
                     </button>
