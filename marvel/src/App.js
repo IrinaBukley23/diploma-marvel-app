aa79185e6c54801dec404f7d873389a4fc4a3b52
Irina <irenserge@mail.ru>
2021-11-29 20:22:40 +0300

    feat: char-list is created

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,7 +1,10 @@
 
 import Header from './components/Header';
 import RandomChar from './components/RandomChar';
+import CharList from './components/CharList';
 import './styles/variables.scss';
+
+import decoration from './resources/vision.png';
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
       <Header/>
       <main>
         <RandomChar/>
+        <div className="char__content">
+          <CharList/>
 
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision"/>
       </main>
     </div>
   );
