d6f0a2d3a3568c2e99cdb92e5ca1737349e36ef3
Irina <irenserge@mail.ru>
2021-11-29 15:00:06 +0300

    feat: random-char ui created

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,12 +1,16 @@
 
 import Header from './components/Header';
+import RandomChar from './components/RandomChar';
 import './styles/variables.scss';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      
+      <main>
+        <RandomChar/>
+
+      </main>
     </div>
   );
 }
