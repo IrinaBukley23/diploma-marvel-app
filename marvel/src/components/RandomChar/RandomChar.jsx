8a8c8a25967b7f722366cbc70f8c5828b014b5e4
Irina <irenserge@mail.ru>
2021-11-29 19:54:32 +0300

    fix: request is corrected

--- marvel/src/components/RandomChar/RandomChar.jsx
+++ marvel/src/components/RandomChar/RandomChar.jsx
@@ -8,11 +8,6 @@ import mjolnir from '../../resources/mjolnir.png';
 
 class RandomChar extends Component {
 
-    constructor(props){
-        super(props);
-        this.updateChar();
-    }
-
     state = {
         char: {},
         loading: true,
@@ -20,6 +15,15 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount(){
+        this.updateChar();
+        this.timerId = setInterval(this.updateChar, 5000);
+    }
+
+    componentWillMount() {
+        clearInterval(this.timerId);
+    }
     
     onCharLoaded = (char) => {
         this.setState( {
